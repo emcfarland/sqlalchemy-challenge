ca9de64c622b0fe34a929f599b2318a56f8004d4
Elliott McFarland <elliottmcf@gmail.com>
2020-11-14 15:46:41 -0500

    Completes queries in ipynb and creates initial routes for Flask app

--- app.py
+++ app.py
@@ -1,1 +1,42 @@
-# App to be created
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Passenger = Base.classes.passenger
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to my project page. Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br>"
+        f"/api/v1.0/stations<br>"
+        f"/api/v1.0/tobs<br>"
+        f"/api/v1.0/<start> and /api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+
+@app.route("/api/v1.0/stations")
+
+@app.route("/api/v1.0/tobs")
+
+@app.route("/api/v1.0/<start>")
+
+@app.route("/api/v1.0/<start>/<end>")
